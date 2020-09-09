1a690ccd23a12a34266c6f90c34e13f0b9c3d5ca
Codeaday <AJORIGAMI1@gmail.com>
2020-09-08 19:58:31 -0500

    Consolidated setup code for converter tests into EntityConverterShould.

---
+++ HackerNews.UnitTests/CommentConverterShould.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using HackerNews.Api.Converters;
+using HackerNews.Domain;
+using HackerNews.Domain.Models;
+using HackerNews.EF;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerNews.UnitTests
+{
+	public class CommentConverterShould : EntityConverterShould
+	{
+
+		public CommentConverterShould()
+		{
+		}
+
+
+		[Fact]
+		public async Task ConvertEntityAsync_Returns_GetModel()
+		{
+			// Act
+			var getModel = await _commentConverter.ConvertEntityAsync<GetCommentModel>(_comment);
+
+			// Assert
+			Assert.IsType<GetCommentModel>(getModel);
+
+			_mockMapper.Verify(m => m.Map<GetCommentModel>(It.IsAny<Comment>()), Times.Once);
+		}
+
+		[Fact]
+		public async Task ConvertEntitiesAsync_Returns_GetModels()
+		{
+			// Arrange
+			int numOfComments = _comments.Count;
+
+			// Act
+			var getModels = await _commentConverter.ConvertEntitiesAsync<GetCommentModel>(_comments);
+
+			// Assert
+			Assert.IsType<List<GetCommentModel>>(getModels);
+
+			_mockMapper.Verify(m => m.Map<GetCommentModel>(It.IsAny<Comment>()), Times.Exactly(numOfComments));
+		}
+
+		[Fact]
+		public async Task ConvertPostModelAsync_Returns_Entity()
+		{
+			// Arrange
+			const int parentArticleId = 1;
+			const int parentCommentId = 2;
+			
+			_parentArticle.Id = parentArticleId;
+			_parentComment.Id = parentCommentId;
+
+			_postCommentModel.ParentArticleId = parentArticleId;
+			_postCommentModel.ParentCommentId = parentCommentId;
+
+
+			_mockCommentRepo.Setup(cr => cr.GetEntityAsync(parentCommentId))
+				.ReturnsAsync(_parentComment);
+			_mockArticleRepo.Setup(cr => cr.GetEntityAsync(parentArticleId))
+				.ReturnsAsync(_parentArticle);
+
+			// Act
+			var comment = await _commentConverter.ConvertEntityModelAsync(_postCommentModel);
+
+			// Assert
+			Assert.IsType<Comment>(comment);
+
+			_mockMapper.Verify(m => m.Map<Comment>(It.IsAny<PostCommentModel>()), Times.Once);
+
+			_mockCommentRepo.Verify(cr => cr.GetEntityAsync(It.IsAny<int>()), Times.Once);
+			_mockArticleRepo.Verify(ar => ar.GetEntityAsync(It.IsAny<int>()), Times.Once);
+
+			Assert.Equal(_parentArticle, comment.ParentArticle);
+			Assert.Equal(_parentComment, comment.ParentComment);
+		}
+	}
+}
---
+++ HackerNews.UnitTests/EntityConverterShould.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using HackerNews.Api.Converters;
+using HackerNews.Domain;
+using HackerNews.Domain.Models;
+using HackerNews.EF;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace HackerNews.UnitTests
+{
+	public abstract class EntityConverterShould
+	{
+		public Article _article;
+		public Comment _comment;
+
+		public Article _parentArticle;
+		public Comment _parentComment;
+
+		public List<Article> _articles;
+		public List<Comment> _comments;
+
+		public PostArticleModel _postArticleModel;
+		public List<PostArticleModel> _postArticleModels;
+
+		public PostCommentModel _postCommentModel;
+		public List<PostCommentModel> _postCommentModels;
+
+		public Mock<IMapper> _mockMapper;
+
+		public Mock<EntityRepository<Comment>> _mockCommentRepo;
+		public Mock<EntityRepository<Article>> _mockArticleRepo;
+
+		public CommentConverter _commentConverter;
+		public ArticleConverter _articleConverter;
+
+		public EntityConverterShould()
+		{
+			_article = new Article();
+			_comment = new Comment();
+
+			_parentArticle = new Article();
+			_parentComment = new Comment();
+
+			_articles = CreateListOfType<Article>(5);
+			_comments = CreateListOfType<Comment>(5);
+
+			_postArticleModel = new PostArticleModel();
+			_postArticleModels = CreateListOfType<PostArticleModel>(5);
+
+			_postCommentModel = new PostCommentModel();
+			_postCommentModels = CreateListOfType<PostCommentModel>(5);
+
+			_mockMapper = new Mock<IMapper>();
+
+			_mockCommentRepo = new Mock<EntityRepository<Comment>>(null);
+			_mockArticleRepo = new Mock<EntityRepository<Article>>(null);
+
+			_commentConverter = new CommentConverter(_mockMapper.Object, _mockCommentRepo.Object, _mockArticleRepo.Object);
+			_articleConverter = new ArticleConverter(_mockMapper.Object);
+
+			SetupDefaultMockMapperBehavior(_mockMapper);
+		}
+
+		private static List<T> CreateListOfType<T>(int number) where T : new()
+		{
+			List<T> list = new List<T>();
+			for (int i = 0; i < number; i++)
+			{
+				list.Add(new T());
+			}
+			return list;
+		}
+
+		private static void SetupDefaultMockMapperBehavior(Mock<IMapper> mockMapper)
+		{
+			mockMapper.Setup(m => m.Map<Article>(It.IsAny<PostArticleModel>())).Returns(new Article());
+			mockMapper.Setup(m => m.Map<Comment>(It.IsAny<PostCommentModel>())).Returns(new Comment());
+
+			mockMapper.Setup(m => m.Map<GetCommentModel>(It.IsAny<Comment>())).Returns(new GetCommentModel());
+			mockMapper.Setup(m => m.Map<GetArticleModel>(It.IsAny<Article>())).Returns(new GetArticleModel());
+		}
+	}
+}
