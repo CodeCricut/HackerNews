2c13fc5f26a9eac6f2b250d687ee14408f2f9419
Andrew Richerson <ajorigami1@gmail.com>
2020-09-29 13:58:36 -0500

    Added basic logging boilerplate and example logs in ArticlesController.

--- HackerNews.Api/Controllers/ArticlesController.cs
+++ HackerNews.Api/Controllers/ArticlesController.cs
@@ -9,6 +9,7 @@ using HackerNews.Domain.Models.Auth;
 using HackerNews.Domain.Models.Users;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -21,15 +22,18 @@ namespace HackerNews.Api.Controllers
 		private readonly ArticleService _articleService;
 		private readonly IVoteableEntityService<Article> _articleVoter;
 		private readonly UserAuthService _userAuthService;
+		private readonly ILogger<ArticlesController> _logger;
 
 		public ArticlesController(
 			ArticleService articleService,
 			IVoteableEntityService<Article> articleVoter,
-			UserAuthService userAuthService)
+			UserAuthService userAuthService,
+			ILogger<ArticlesController> logger)
 		{
 			_articleService = articleService;
 			_articleVoter = articleVoter;
 			_userAuthService = userAuthService;
+			_logger = logger;
 		}
 
 		#region Create
@@ -37,6 +41,9 @@ namespace HackerNews.Api.Controllers
 		[Authorize]
 		public async Task<IActionResult> PostArticleAsync([FromBody] PostArticleModel articleModel)
 		{
+			_logger.LogInformation("PostArticleAsync called.");
+			if (articleModel == null) _logger.LogWarning("Post model null.");
+
 			if (!ModelState.IsValid) throw new InvalidPostException(ModelState);
 
 			var user = await _userAuthService.GetAuthenticatedUser(HttpContext);
@@ -50,6 +57,9 @@ namespace HackerNews.Api.Controllers
 		[Authorize]
 		public async Task<IActionResult> PostArticlesAsync([FromBody] List<PostArticleModel> articleModels)
 		{
+			_logger.LogInformation("PostArticlesAsync called.");
+			if (articleModels == null) _logger.LogWarning("Post models null.");
+
 			if (!ModelState.IsValid) throw new InvalidPostException(ModelState);
 
 			var user = await _userAuthService.GetAuthenticatedUser(HttpContext);
@@ -64,6 +74,7 @@ namespace HackerNews.Api.Controllers
 		[EnableQuery]
 		public async Task<IActionResult> GetArticlesAsync()
 		{
+			_logger.LogInformation("GetArticlesAsync called.");
 			var articleModels = await _articleService.GetAllEntityModelsAsync();
 			return Ok(articleModels);
 		}
@@ -71,6 +82,8 @@ namespace HackerNews.Api.Controllers
 		[EnableQuery]
 		public async Task<IActionResult> GetArticleAsync(int key)
 		{
+			_logger.LogInformation("GetArticleAsync called.");
+
 			var articleModel = await _articleService.GetEntityModelAsync(key);
 
 			return Ok(articleModel);
@@ -82,6 +95,10 @@ namespace HackerNews.Api.Controllers
 		[Authorize]
 		public async Task<IActionResult> PutArticleAsync(int id, [FromBody] PostArticleModel articleModel)
 		{
+			_logger.LogInformation("PutArticleAsync called.");
+			if (id < 1) _logger.LogWarning("Invalid id provided or id didn't bind.");
+			if (articleModel == null) _logger.LogWarning("Model null.");
+
 			if (!ModelState.IsValid) throw new InvalidPostException(ModelState);
 
 			var user = await _userAuthService.GetAuthenticatedUser(HttpContext);
@@ -95,6 +112,9 @@ namespace HackerNews.Api.Controllers
 		[Authorize]
 		public async Task<IActionResult> VoteArticleAsync(int articleId, [FromBody] bool upvote)
 		{
+			_logger.LogInformation("VoteArticleAsync called.");
+			if (articleId < 1) _logger.LogWarning("Invalid id provided or id didn't bind.");
+
 			if (!ModelState.IsValid) throw new InvalidPostException(ModelState);
 
 			var user = await _userAuthService.GetAuthenticatedUser(HttpContext);
@@ -110,6 +130,9 @@ namespace HackerNews.Api.Controllers
 		[Authorize]
 		public async Task<IActionResult> DeleteArticleAsync(int key)
 		{
+			_logger.LogInformation("DeleteArticleAsync called.");
+			if (key < 1) _logger.LogWarning("Invalid id provided or id didn't bind.");
+
 			if (!ModelState.IsValid) throw new InvalidPostException(ModelState);
 
 			var user = await _userAuthService.GetAuthenticatedUser(HttpContext);
