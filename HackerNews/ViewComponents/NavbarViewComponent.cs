36cac451f9da9de52d76804587190bb2caf8d0f0
Andrew Richerson <ajorigami1@gmail.com>
2020-10-20 03:31:36 -0500

    merged frontend.

---
+++ HackerNews/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using HackerNews.Domain.Models.Users;
+using HackerNews.Helpers.ApiServices.Interfaces;
+using HackerNews.ViewModels.Other;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackerNews.ViewComponents
+{
+	public class NavbarViewComponent : ViewComponent
+	{
+		private readonly IApiReader _apiReader;
+
+		public NavbarViewComponent(IApiReader apiReader)
+		{
+			_apiReader = apiReader;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var privateUser = await _apiReader.GetEndpointAsync<GetPrivateUserModel>("users/me");
+
+			var model = new NavbarViewModel { User = privateUser };
+
+			return View(model);
+		}
+	}
+}
---
+++ HackerNews/ViewModels/Other/CommentCardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HackerNews.Domain.Models.Articles;
+using HackerNews.Domain.Models.Board;
+using HackerNews.Domain.Models.Comments;
+using HackerNews.Domain.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackerNews.ViewModels.Other
+{
+	public class CommentCardViewModel
+	{
+
+		public GetCommentModel Comment { get; set; }
+		public GetArticleModel ParentArticle { get; set; }
+		public GetCommentModel ParentComment { get; set; }
+		public GetBoardModel Board { get; set; }
+		public GetPublicUserModel User { get; set; }
+	}
+}
