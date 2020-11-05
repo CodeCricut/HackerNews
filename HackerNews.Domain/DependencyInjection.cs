ab386e5cccfdc0bd2fee2ef035f5ff01f902909d
Andrew Richerson <ajorigami1@gmail.com>
2020-11-05 13:42:44 -0600

    Fixed the development project. Changes made before switching to the blazor branch were resolved in that branch, so they had to be fixed here as well.

---
+++ HackerNews.Domain/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HackerNews.Domain.Common.Mappings;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HackerNews.Domain
+{
+	public static class DepenedencyInjection
+	{
+		public static IServiceCollection AddDomain(this IServiceCollection services)
+		{
+			//var mapperConfig = new MapperConfiguration(cfg =>
+			//{
+			//	cfg.AddProfile<MappingProfile>();
+			//});
+
+			services.AddAutoMapper(Assembly.GetAssembly(typeof(MappingProfile)));
+			return services;
+		}
+	}
+}
