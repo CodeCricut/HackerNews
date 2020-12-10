f3e237013bd3dc7a724d5be10afa0db3a674eef9
Andrew Richerson <ajorigami1@gmail.com>
2020-12-09 20:48:28 -0600

    Added a few missing files that should have been part of last commit.

---
+++ HackerNews.Application/Users/Queries/GetUserByUsername/GetUserByUsernameQuery.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using HackerNews.Application.Common.Interfaces;
+using HackerNews.Application.Common.Requests;
+using HackerNews.Domain.Common.Models.Users;
+using HackerNews.Domain.Exceptions;
+using HackerNews.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HackerNews.Application.Users.Queries.GetUserByUsername
+{
+	public class GetUserByUsernameQuery : IRequest<GetPublicUserModel>
+	{
+		public GetUserByUsernameQuery(string username)
+		{
+			Username = username;
+		}
+
+		public string Username { get; }
+	}
+
+	public class GetUserByUsernameHandler : DatabaseRequestHandler<GetUserByUsernameQuery, GetPublicUserModel>
+	{
+		public GetUserByUsernameHandler(IUnitOfWork unitOfWork, IMediator mediator, IMapper mapper, ICurrentUserService currentUserService) : base(unitOfWork, mediator, mapper, currentUserService)
+		{
+		}
+
+		public override async Task<GetPublicUserModel> Handle(GetUserByUsernameQuery request, CancellationToken cancellationToken)
+		{
+			var users = await UnitOfWork.Users.GetEntitiesAsync();
+
+			var user = users.FirstOrDefault(u => u.Username == request.Username);
+
+			if (user == null || user.Id <= 0) throw new NotFoundException();
+
+			return Mapper.Map<GetPublicUserModel>(user);
+		}
+	}
+}
