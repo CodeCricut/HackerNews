e5eb6e658fb3b4ca7198ddfbf47c97b17b630179
Andrew Richerson <ajorigami1@gmail.com>
2020-12-12 21:01:36 -0600

    Added image models.

---
+++ HackerNews.Domain/Common/Models/Images/GetImageModel.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HackerNews.Domain.Common.Mappings;
+using HackerNews.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerNews.Domain.Common.Models.Images
+{
+	public class GetImageModel : GetModelDto, IMapFrom<Image>
+	{
+		public string ImageTitle { get; set; }
+		public string ImageDescription { get; set; }
+		public byte[] ImageData { get; set; }
+
+	}
+}
---
+++ HackerNews.Domain/Common/Models/Images/PostImageModel.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HackerNews.Domain.Common.Mappings;
+using HackerNews.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerNews.Domain.Common.Models.Images
+{
+	public class PostImageModel : PostModelDto, IMapFrom<Image>
+	{
+		public string ImageTitle { get; set; }
+		public string ImageDescription { get; set; }
+		public byte[] ImageData { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<PostImageModel, Image>();
+		}
+	}
+}
