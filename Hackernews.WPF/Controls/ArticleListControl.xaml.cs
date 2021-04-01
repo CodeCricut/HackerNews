77c25713a0eea150f80236591498a82287b6f726
Andrew Richerson <ajorigami1@gmail.com>
2021-04-01 12:29:16 -0500

    Extracted articles list control.

---
+++ Hackernews.WPF/Controls/ArticleListControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Hackernews.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hackernews.WPF.Controls
+{
+	/// <summary>
+	/// Interaction logic for ArticleListControl.xaml
+	/// </summary>
+	public partial class ArticleListControl : UserControl
+	{
+		public ArticlesViewModel ArticlesViewModel
+		{
+			get { return (ArticlesViewModel)GetValue(ArticlesViewModelProperty); }
+			set { SetValue(ArticlesViewModelProperty, value); }
+		}
+
+		public static readonly DependencyProperty ArticlesViewModelProperty =
+			DependencyProperty.Register("ArticlesViewModel", typeof(ArticlesViewModel), typeof(ArticleListControl));
+
+
+
+		public ArticleListControl()
+		{
+			InitializeComponent();
+
+			rootElement.DataContext = this;
+		}
+	}
+}
