8a497bc385640d30fd65e923ba0c282ef3a3825b
Andrew Richerson <ajorigami1@gmail.com>
2021-04-16 07:43:20 -0500

    Cleaned up main vm.

--- Hackernews.WPF/MVVM/ViewModel/MainWindowViewModel.cs
+++ Hackernews.WPF/MVVM/ViewModel/MainWindowViewModel.cs
@@ -12,19 +12,48 @@ namespace Hackernews.WPF.ViewModels
 {
 	public class MainWindowViewModel : BaseViewModel
 	{
+		public Action? CloseAction { get; set; }
+		public ICommand CloseCommand { get; }
+
+		public Action? LogoutAction { get; set; }
+		public ICommand LogoutCommand { get; }
+
+		public PrivateUserViewModel PrivateUserViewModel { get; }
+
+		#region Select view commands
+		public ICommand SelectHomeCommand { get; }
+		public ICommand SelectProfileCommand { get; }
+		public ICommand SelectSettingsCommand { get; }
+
+		public AsyncDelegateCommand SelectUsersCommand { get; }
+		public ICommand SelectBoardsCommand { get; }
+		public ICommand SelectArticlesCommand { get; }
+		public ICommand SelectCommentsCommand { get; }
+		#endregion
+
+		#region Fullscreen VMs
 		public bool NotInFullscreenMode { get => SelectedFullscreenViewModel == null; }
+
 		private object _selectedFullscreenViewModel;
 		public object SelectedFullscreenViewModel
 		{
 			get => _selectedFullscreenViewModel;
 			set
 			{
+				SelectedListViewModel = null;
+				SelectedDetailsViewModel = null;
 				_selectedFullscreenViewModel = value;
 				RaisePropertyChanged();
 				RaisePropertyChanged(nameof(NotInFullscreenMode));
 			}
 		}
 
+		public HomeViewModel HomeViewModel { get; }
+		public ProfileViewModel ProfileViewModel { get; }
+		public SettingsViewModel SettingsViewModel { get; }
+		#endregion
+
+		#region List VMs
 		private IPageNavigatorViewModel _selectedListViewModel;
 		public IPageNavigatorViewModel SelectedListViewModel
 		{
@@ -36,6 +65,13 @@ namespace Hackernews.WPF.ViewModels
 			}
 		}
 
+		public BoardsListViewModel BoardListViewModel { get; }
+		public ArticleListViewModel ArticleListViewModel { get; }
+		public CommentListViewModel CommentListViewModel { get; }
+		public UserListViewModel UserListViewModel { get; }
+		#endregion
+
+		#region Details VMs
 		private object _selectedDetailsViewModel;
 		public object SelectedDetailsViewModel
 		{
@@ -47,40 +83,11 @@ namespace Hackernews.WPF.ViewModels
 			}
 		}
 
-		public Action? CloseAction { get; set; }
-		public ICommand CloseCommand { get; }
-
-		public Action? LogoutAction { get; set; }
-		public ICommand LogoutCommand { get; }
-
-
-		// TODO: consolidate into one command with a command parameter
-		public ICommand SelectHomeCommand { get; }
-		public ICommand SelectProfileCommand { get; }
-		public ICommand SelectSettingsCommand { get; }
-
-
-		public AsyncDelegateCommand SelectUsersCommand { get; }
-		public ICommand SelectBoardsCommand { get; }
-		public ICommand SelectArticlesCommand { get; }
-		public ICommand SelectCommentsCommand { get; }
-
-		public BoardsListViewModel BoardListViewModel { get; }
-		public ArticleListViewModel ArticleListViewModel { get; }
-		public CommentListViewModel CommentListViewModel { get; }
-		public UserListViewModel UserListViewModel { get; }
-
-		public HomeViewModel HomeViewModel { get; }
-		public ProfileViewModel ProfileViewModel { get; }
-		public SettingsViewModel SettingsViewModel { get; }
-
 		public BoardViewModel BoardViewModel { get; }
 		public ArticleViewModel ArticleViewModel { get; }
 		public CommentViewModel CommentViewModel { get; }
 		public PublicUserViewModel PublicUserViewModel { get; }
-
-		//public NavigationViewModel NavigationViewModel { get; }
-		public PrivateUserViewModel PrivateUserViewModel { get; }
+		#endregion
 
 		public MainWindowViewModel(IApiClient apiClient, PrivateUserViewModel userVM)
 		{
@@ -106,8 +113,6 @@ namespace Hackernews.WPF.ViewModels
 			ArticleViewModel = new ArticleViewModel(userVM);
 			CommentViewModel = new CommentViewModel();
 
-			//NavigationViewModel = new NavigationViewModel(UserListViewModel, BoardsListViewModel, ArticleListViewModel, CommentListViewModel);
-
 			SelectHomeCommand = new DelegateCommand(SelectHome);
 			SelectProfileCommand = new DelegateCommand(SelectProfile);
 			SelectSettingsCommand = new DelegateCommand(SelectSettings);
@@ -120,8 +125,7 @@ namespace Hackernews.WPF.ViewModels
 
 		public void SelectHome(object parameter = null)
 		{
-			SelectedListViewModel = null;
-			SelectedDetailsViewModel = null;
+			
 			SelectedFullscreenViewModel = HomeViewModel;
 		}
 
