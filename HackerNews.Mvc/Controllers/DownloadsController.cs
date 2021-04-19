a1fd834dfbfa327f2277db968644ddd64979eb1e
Andrew Richerson <ajorigami1@gmail.com>
2021-04-19 10:55:10 -0500

    Updated build settings for wpf and added test download link to mvc proj.

---
+++ HackerNews.Mvc/Controllers/DownloadsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackerNews.Mvc.Controllers
+{
+	public class DownloadsController : FrontendController
+	{
+		private readonly IWebHostEnvironment _environment;
+
+		public DownloadsController(IWebHostEnvironment environment)
+		{
+			_environment = environment;
+		}
+
+		public FileContentResult Wpf()
+		{
+			try
+			{
+				var file = System.IO.File.ReadAllBytes(@"C:\Users\ajori\Pictures\Saved Pictures\walrus.jpg");
+				return new FileContentResult(file, "image/jpeg");
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+		}
+	}
+}
