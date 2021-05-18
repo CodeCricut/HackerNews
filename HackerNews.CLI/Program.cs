c431784ebbe7d4837b74da344fa7bc194d3dd808
Andrew Richerson <ajorigami1@gmail.com>
2021-05-18 06:11:59 -0500

    Set up project to use CLI architecture.

--- HackerNews.CLI/Program.cs
+++ HackerNews.CLI/Program.cs
@@ -1,12 +1,101 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace HackerNews.CLI
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static string[] _args = new string[0];
+		static async Task<int> Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			_args = args;
+
+			IHostBuilder builder = BuildHostBuilder();
+
+			using IHost host = builder.Build();
+			try
+			{
+				// Builds and starts the host, but will end when CTRL+C is entered.
+				// If the CTRL+C to shutdown process is not desired, then use builder.StartAsync()
+				//await builder.RunConsoleAsync();
+
+				await host.StartAsync();
+			}
+			catch (Exception ex)
+			{
+				var logger = host?.Services.GetService<ILogger<Program>>();
+				if (logger != null)
+				{
+					logger.LogError(ex.Message);
+				}
+				else
+				{
+					Console.WriteLine($"FATAL ERROR: {ex.Message}");
+				}
+				return 1;
+			}
+
+			return 0;
+		}
+
+		/// <summary>
+		/// Build a host responsible for encapsulating app resources such as:
+		///		*	- services
+		///		*	- logging
+		///		*	- configuration
+		///		*	- hosted services(where hosted services are tasks that have an entry and exit procedure)
+		/// </summary>
+		/// <returns></returns>
+		private static IHostBuilder BuildHostBuilder()
+		{
+			var builder = new HostBuilder()
+				.ConfigureAppConfiguration(Configure)
+				.ConfigureServices(ConfigureServices);
+			return builder;
+		}
+
+		/// <summary>
+		/// Set up the configuration for the host. This is where you should configure things such as files and environment
+		/// variables to be included.
+		/// </summary>
+		private static void Configure(IConfigurationBuilder configBuilder)
+		{
+			configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json", optional: false, reloadOnChange: true)
+				.AddEnvironmentVariables();
+		}
+
+		/// <summary>
+		/// Register services for the host.
+		/// </summary>
+		private static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+		{
+			// Configure app to use a custom shutdown timeout
+			services.Configure((Action<HostOptions>)(option =>
+			{
+				// Default is 5; If shutdown is requested and hosted services don't stop before the timeout, they will be forced to stop
+				option.ShutdownTimeout = System.TimeSpan.FromSeconds(3);
+			}));
+
+			// The host has access to the configuration.
+			var configuration = hostContext.Configuration;
+
+			services.AddSerilogLogger(configuration);
+
+			AddProgramServices();
+		}
+
+		/// <summary>
+		/// Add the main program services responsible for doing things based on CLI args.
+		/// </summary>
+		private static void AddProgramServices()
+		{
+
 		}
 	}
 }
---
+++ HackerNews.CLI/ServiceCollectionExtensions.cs
@@ -0,0 +1,87 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System;
+
+namespace HackerNews.CLI
+{
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// If an instance of <typeparamref name="TProgramOptions"/> could be constructed from the <paramref name="args"/>, 
+		/// then register the <typeparamref name="TProgramEntry"/>
+		/// which is dependant upon the <typeparamref name="TProgramOptions"/> as a hosted service.
+		/// </summary>
+		/// <typeparam name="TProgramEntry"></typeparam>
+		/// <typeparam name="TProgramOptions"></typeparam>
+		/// <param name="services"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static IServiceCollection AddProgramService<TProgramEntry, TProgramOptions>(this IServiceCollection services, string[] args)
+			where TProgramEntry : class, IHostedService
+			where TProgramOptions : class
+		{
+			if (services.TryAddProgramOption<TProgramOptions>(args))
+			{
+				return services.AddHostedService<TProgramEntry>();
+			}
+			else return services;
+		}
+
+
+		/// <summary>
+		/// Try to parse the <paramref name="args"/> to construct the relevant <typeparamref name="TOption"/> and register it to the service collection.
+		/// If the options couldn't be constructed, then the service collection will not be modified.
+		/// <see cref="AddProgramService{TProgramEntry, TProgramOptions}(IServiceCollection, string[])"/> is preferable.
+		/// </summary>
+		/// <typeparam name="TOption">The type of option to try to register.</typeparam>
+		/// <param name="services">The service collection to add to.</param>
+		/// <param name="args">The CLI arguments to parse.</param>
+		/// <returns>The <paramref name="args"/> could be parsed and the <typeparamref name="TOption"/> was registerd with the service collection.</returns>
+		public static bool TryAddProgramOption<TOption>(this IServiceCollection services, string[] args)
+			where TOption : class
+		{
+			bool added = false;
+
+			new Parser().ParseArguments(args, new[] { typeof(TOption) })
+				.WithParsed(opt =>
+				{
+					services.AddSingleton<TOption>((TOption)opt);
+					added = true;
+				})
+				.WithNotParsed(opt => added = false);
+
+			return added;
+		}
+
+		/// <summary>
+		/// Register the Serilog <see cref="ILogger{TCategoryName}"/> dependency and configure it using the <paramref name="configuration"/>.
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="configuration"></param>
+		/// <returns>The service collection for chaining.</returns>
+		public static IServiceCollection AddSerilogLogger(this IServiceCollection services, IConfiguration configuration)
+		{
+			var logger = new LoggerConfiguration()
+						.ReadFrom.Configuration(configuration)
+						.Enrich.FromLogContext()
+						.CreateLogger();
+
+			return services.AddLogging(config =>
+			{
+				config.ClearProviders();
+				config.AddProvider(new SerilogLoggerProvider(logger));
+				string minimumLevel = configuration.GetSection("Serilog:MinimumLevel")?.Value;
+
+				if (!string.IsNullOrEmpty(minimumLevel))
+				{
+					config.SetMinimumLevel(Enum.Parse<LogLevel>(minimumLevel));
+				}
+			});
+		}
+	}
+}
