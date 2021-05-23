ce4ff497b8f7740a87485a30af93aea91770f2ba
Andrew Richerson <ajorigami1@gmail.com>
2021-05-22 22:23:15 -0500

    backup, debating redesign possibilities.

---
+++ HackerNews.CLI/Requests/GetEntityById/GetEntityByIdRequestBuilder.cs
@@ -0,0 +1,32 @@
+﻿using HackerNews.CLI.Verbs.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerNews.CLI.Requests.GetEntityById
+{
+	public class GetEntityByIdRequestBuilder<TGetModel, TInclusionConfig> :
+		IRequestBuilder<GetEntityByIdRequest<TGetModel, TInclusionConfig>>
+	{
+
+		public List<Action> BuildActions { get; }
+
+		public IdRequestConfiguration<
+				GetEntityByIdRequestBuilder<TGetModel, TInclusionConfig>,
+				GetEntityByIdRequest<TGetModel, TInclusionConfig>> 
+			OverrideId { get; }
+
+		public IBoardInclusionRequestConfiguration<GetBoardByIdRequestBuilder, GetBoardByIdRequest> OverrideInclusion { get; }
+
+		public IVerbosityRequestConfiguration<GetBoardByIdRequestBuilder, GetBoardByIdRequest> OverrideVerbosity { get; }
+
+		public IPrintRequestConfiguration<GetBoardByIdRequestBuilder, GetBoardByIdRequest> OverridePrint { get; }
+
+		public IFileRequestConfiguration<GetBoardByIdRequestBuilder, GetBoardByIdRequest> OverrideFile { get; }
+
+		public GetEntityByIdRequest<TGetModel, TInclusionConfig> Build()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
