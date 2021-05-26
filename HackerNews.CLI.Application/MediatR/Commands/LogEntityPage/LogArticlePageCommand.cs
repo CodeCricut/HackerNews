a3306235bf0f87a8d3f5fe4b863bcf115b6c014f
Andrew Richerson <ajorigami1@gmail.com>
2021-05-25 19:13:27 -0500

    Added log article page commands.

---
+++ HackerNews.CLI.Application/MediatR/Commands/LogEntityPage/LogArticlePageCommand.cs
@@ -0,0 +1,25 @@
+﻿using HackerNews.CLI.Loggers;
+using HackerNews.CLI.MediatR.Commands.LogEntities;
+using HackerNews.CLI.Verbs.GetEntity;
+using HackerNews.Domain.Common.Models;
+using HackerNews.Domain.Common.Models.Articles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerNews.CLI.Application.MediatR.Commands.LogEntityPage
+{
+	public class LogArticlePageCommand : LogEntityPageCommand<GetArticleModel>
+	{
+		public LogArticlePageCommand(PaginatedList<GetArticleModel> entities, IPrintOptions printOptions) : base(entities, printOptions)
+		{
+		}
+	}
+
+	public class LogArticlePageCommandHandler : LogEntityPageCommandHandler<LogArticlePageCommand, GetArticleModel>
+	{
+		public LogArticlePageCommandHandler(IEntityLogger<GetArticleModel> entityLogger) : base(entityLogger)
+		{
+		}
+	}
+}
