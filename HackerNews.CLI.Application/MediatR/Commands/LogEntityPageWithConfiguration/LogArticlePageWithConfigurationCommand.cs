5d6f7a7c0d59e418be7fc1dd5203c1e0922c5209
Andrew Richerson <ajorigami1@gmail.com>
2021-05-25 19:27:01 -0500

    Updated log entity page commands to use new class.

---
+++ HackerNews.CLI.Application/MediatR/Commands/LogEntityPageWithConfiguration/LogArticlePageWithConfigurationCommand.cs
@@ -0,0 +1,32 @@
+﻿using HackerNews.CLI.InclusionConfiguration;
+using HackerNews.CLI.Loggers;
+using HackerNews.CLI.MediatR.Commands.LogEntities;
+using HackerNews.CLI.Options;
+using HackerNews.CLI.Util;
+using HackerNews.CLI.Verbs.GetEntity;
+using HackerNews.Domain.Common.Models;
+using HackerNews.Domain.Common.Models.Articles;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HackerNews.CLI.Application.MediatR.Commands.LogEntityPage
+{
+	public class LogArticlePageWithConfigurationCommand : LogEntityPageCommand<GetArticleModel, ArticleInclusionConfiguration>
+	{
+		public LogArticlePageWithConfigurationCommand(PaginatedList<GetArticleModel> entityPage, IPrintOptions printOptions, ArticleInclusionConfiguration inclusionConfig) : base(entityPage, printOptions, inclusionConfig)
+		{
+		}
+	}
+
+	public class LogArticlePageWithConfigurationCommandHandler
+		: LogEntityPageCommandHandler<LogArticlePageWithConfigurationCommand, GetArticleModel, ArticleInclusionConfiguration>
+	{
+		public LogArticlePageWithConfigurationCommandHandler(IConfigurableEntityLogger<GetArticleModel, ArticleInclusionConfiguration> configEntityLogger) : base(configEntityLogger)
+		{
+		}
+	}
+}
---
+++ HackerNews.CLI.Application/MediatR/Commands/LogEntityPageWithConfiguration/LogBoardPageWithConfigurationCommand.cs
@@ -0,0 +1,30 @@
+﻿using HackerNews.CLI.Application.MediatR.Commands.LogEntityPage;
+using HackerNews.CLI.InclusionConfiguration;
+using HackerNews.CLI.Loggers;
+using HackerNews.CLI.MediatR.Commands.LogEntities;
+using HackerNews.CLI.Util;
+using HackerNews.CLI.Verbs.GetBoards;
+using HackerNews.CLI.Verbs.GetEntity;
+using HackerNews.Domain.Common.Models;
+using HackerNews.Domain.Common.Models.Boards;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HackerNews.CLI.MediatR.Commands.LogEntityPage
+{
+	public class LogBoardPageWithConfigurationCommand : LogEntityPageCommand<GetBoardModel, BoardInclusionConfiguration>
+	{
+		public LogBoardPageWithConfigurationCommand(PaginatedList<GetBoardModel> entityPage, IPrintOptions printOptions, BoardInclusionConfiguration inclusionConfig) : base(entityPage, printOptions, inclusionConfig)
+		{
+		}
+	}
+
+	public class LogBoardPageWithConfigurationCommandHandler :
+		LogEntityPageCommandHandler<LogBoardPageWithConfigurationCommand, GetBoardModel, BoardInclusionConfiguration>
+	{
+		public LogBoardPageWithConfigurationCommandHandler(IConfigurableEntityLogger<GetBoardModel, BoardInclusionConfiguration> configEntityLogger) : base(configEntityLogger)
+		{
+		}
+	}
+}
