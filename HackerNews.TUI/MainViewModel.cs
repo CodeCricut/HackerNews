239eb31834873db799f0a77d9eef3cd3854cbf44
Andrew Richerson <ajorigami1@gmail.com>
2021-06-12 15:23:57 -0500

    Added basic TUI proj.

---
+++ HackerNews.TUI/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleFramework.Events;
+using HackerNews.WPF.Core.ViewModel;
+
+namespace HackerNews.TUI
+{
+	public class MainViewModel : BaseViewModel
+	{
+		public ICommand ChangeTextCommand { get; init; }
+
+		private string _myText;
+
+		public string MyText
+		{
+			get { return _myText; }
+			set { Set(ref _myText, value); }
+		}
+
+		public MainViewModel()
+		{
+			MyText = "Starting text";
+			ChangeTextCommand = new RelayCommand(
+				_ => MyText = "new text");
+		}
+	}
+}
---
+++ HackerNews.TUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleFramework;
+using ConsoleFramework.Controls;
+using System;
+
+namespace HackerNews.TUI
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var vm = new MainViewModel();
+
+			var windowsHost = new WindowsHost();
+			Window mainWindow = (Window)ConsoleApplication.LoadFromXaml("HackerNews.TUI.main.xml", vm);
+			windowsHost.Show(mainWindow);
+			ConsoleApplication.Instance.Run(windowsHost);
+		}
+	}
+}
